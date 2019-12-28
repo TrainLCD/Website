c5b5c35c1ac5191e14310344ba4b7f893d6b1c70
TinyKitten <catwalk8205@gmail.com>
2019-12-29 02:08:22 +0900

    雑に動画追加

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,13 @@
 <script>
     import Header from '../components/header.svelte';
+
+    function onNextClick() {
+        const nextElem = document.querySelector('#next');
+        window.scrollTo({
+            top: nextElem.getBoundingClientRect().top,
+            behavior: 'smooth'
+        });
+    }
 </script>
 
 <svelte:head>
@@ -9,12 +17,18 @@
 <style>
     section {
         display: flex;
-        justify-content: center;
-        align-items: center;
         flex-direction: column;
         min-height: 100vh;
         padding: 32px;
         box-sizing: border-box;
+    }
+
+    .horizonalCenter {
+        align-items: center;
+    }
+
+    .verticalCenter {
+        justify-content: center;
     }
 
     .mockup {
@@ -64,17 +78,43 @@
         color: #aaa;
     }
 
+    .arrow {
+        position: absolute;
+        left: auto;
+        right: auto;
+        bottom: 32px;
+    }
+
+    figcaption {
+        text-align: center;
+        margin-top: 12px;
+        color: #888;
+    }
+
+    .heading {
+        margin-top: 64px;
+        font-size: 2rem;
+        color: #555;
+        border-bottom: 4px solid #555;
+        padding-bottom: 12px;
+        margin-bottom: 48px;
+    }
+
     /* Oppa PC Style */
     @media (min-width: 800px) {
         .mockup {
             max-width: 40%;
+        }
+
+        .arrow {
+            cursor: pointer;
         }
     }
 </style>
 
 <Header/>
 
-<section>
+<section class="verticalCenter horizonalCenter">
     <img class="mockup" src="/images/mock.png" alt="Mockup"/>
     <h1 class="appNameTitle">TrainLCD</h1>
     <h2 class="appDescription">電車のLCDを再現したスマホアプリです</h2>
@@ -82,13 +122,25 @@
         <a href="https://apps.apple.com/jp/app/trainlcd/id1486355943" target="_blank" rel="noopener noreferrer">
             <img src="/images/appStore.svg" alt="App Store">
         </a>
-        <a href="https://play.google.com/store/apps/details?id=me.tinykitten.trainlcd" target="_blank" rel="noopener noreferrer">
+        <a href="https://play.google.com/store/apps/details?id=me.tinykitten.trainlcd" target="_blank"
+           rel="noopener noreferrer">
             <img src="/images/googlePlay.png" alt="Google Play">
         </a>
     </div>
+
+    <img on:click={onNextClick} class="arrow" src="/images/arrow.svg" alt="Next">
 </section>
 
-<footer id="next">
+<section id="next" class="horizonalCenter">
+    <h1 class="heading">GALLERY</h1>
+    <figure>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/wxmmoxxss10" frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <figcaption>実際に使っている様子</figcaption>
+    </figure>
+</section>
+
+<footer>
     <h3><a class="footerLink" href="https://tinykitten.me" target="_blank" rel="noopener noreferrer">The TinyKitten
         Project</a></h3>
     <p class="footerCopy">&copy; TinyKitten 2019</p>
