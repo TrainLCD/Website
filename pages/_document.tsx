c40f3ac0346257ec2c32bab2cf8eb4afe7efc08d
TinyKitten <ts@tinykitten.me>
2020-12-22 22:49:48 +0900

    lintfix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,23 @@
+import React from 'react';
 import Document, {
   DocumentContext,
   Html,
   Head,
   Main,
   NextScript,
+  DocumentInitialProps,
 } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
 
     return initialProps;
   }
 
-  render() {
+  render(): React.ReactElement {
     return (
       <Html lang="ja">
         <Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home(): React.ReactElement {
