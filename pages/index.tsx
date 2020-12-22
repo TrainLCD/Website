52d5483c72ac6eb1bbd2846521498e4a6a88a8f2
TinyKitten <ts@tinykitten.me>
2020-12-22 22:35:08 +0900

    code quality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home(): React.ReactElement {
   return (
     <div className={styles.container}>
       <Head>
@@ -61,5 +62,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
