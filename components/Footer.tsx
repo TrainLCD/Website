c285eaebd98cac26f0c0480042d83880ecbd1bc7
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-02-15 14:39:40 +0900

    プラポリをトップに追加

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 import styles from '../styles/components/Footer.module.css';
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.root}>
+      <Link href="/privacy-policy" passHref>
+        <a className={styles.link}>プライバシーポリシー</a>
+      </Link>
       <p className={styles.copyright}>Copyright © TinyKitten 2020</p>
     </footer>
   );
