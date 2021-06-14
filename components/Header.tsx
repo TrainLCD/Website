1aa6a9a007d8469c080277722adf1e95fbce842d
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-06-14 11:44:33 +0900

    ヘッダーアイコンからポートフォリオに飛ばす

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,18 @@
 import styles from '../styles/components/Header.module.css';
-import Link from 'next/link';
 import TinyKittenIcon from './TinyKittenIcon';
 import React from 'react';
-
-const ForwardedIcon = React.forwardRef<HTMLAnchorElement>((props, ref) => (
-  <a {...props} ref={ref}>
-    <TinyKittenIcon width={32} height={32} />
-  </a>
-));
-
-ForwardedIcon.displayName = 'TinyKiten';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.title}>
-        <Link href="/" passHref>
-          <ForwardedIcon />
-        </Link>
+        <a
+          href="https://tinykitten.me"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <TinyKittenIcon width={32} height={32} />
+        </a>
       </div>
     </header>
   );
