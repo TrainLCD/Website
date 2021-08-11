4fee193ee310f1ce7c98d3c3f8378589094d926a
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-08-12 08:29:36 +0900

    公式ツイッターをフッターに

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,14 @@ import { isJa } from '../utils/isJa';
 const Footer: React.FC = () => {
   return (
     <footer className={styles.root}>
+      <a
+        href="https://twitter.com/TrainLCD"
+        rel="noreferrer noopener"
+        className={[styles.link, styles.bold].join(' ')}
+      >
+        {isJa ? '公式Twitter' : 'Twitter(Japanese only)'}
+      </a>
+
       <Link href={isJa ? '/privacy-policy' : '/privacy-policy-en'} passHref>
         <a className={styles.link}>
           {isJa ? 'プライバシーポリシー' : 'Privacy Policy'}
