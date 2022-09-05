94dfa757465254e6c286cbb56fcb95c060fefde2
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-09-05 11:21:36 +0900

    weight 500いらない

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -53,7 +53,7 @@ class MyDocument extends Document {
             crossOrigin=""
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
             rel="stylesheet"
           />
 
