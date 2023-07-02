b9abbb6b1bb3135c9c7dd3343aa3dcf14e548778
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-07-02 17:43:20 +0900

    YouTubeのiframe見切れてた

--- components/anniversary/4th/HistoryPanel.tsx
+++ components/anniversary/4th/HistoryPanel.tsx
@@ -183,9 +183,15 @@ const VisualContainer = styled.div`
 `;
 
 const YouTubeWrapper = styled.div`
+  position: relative;
   width: 100%;
+  height: 0;
+  padding-top: 56.25%;
 
   iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
   }
