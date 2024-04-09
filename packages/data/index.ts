eba67b66e0286c28a68c2da79cf0e3cb1e12a2c2
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-04-09 13:47:05 +0900

    稼働状況更新

--- packages/data/index.ts
+++ packages/data/index.ts
@@ -43,13 +43,11 @@ export const services: Service[] = [
   {
     id: 3,
     name: "フィードバックシステム",
-    status: "maintenance",
-    descriptionEn:
-      "The feedback function is currently under maintenance and is not available.",
-    descriptionJa:
-      "フィードバック機能はメンテナンス中のためご利用になれません。",
-    statusSince: "2024-04-05T00:00:00.000Z",
-    updatedAt: "2024-04-05T00:00:00.000Z",
+    status: "operational",
+    descriptionEn: "The feedback function is currently working properly.",
+    descriptionJa: "フィードバック機能は現在正常にご利用いただけます。",
+    statusSince: "2024-04-09T00:00:00.000Z",
+    updatedAt: "2024-04-09T00:00:00.000Z",
   },
   {
     id: 4,
@@ -86,6 +84,18 @@ export type IncidentHistory = {
 
 export const incidentHistories: IncidentHistory[] = [
   {
+    id: "urn:uuid:e0f56f6e-1855-4ddf-8bc6-d218871d441c",
+    slug: "2024-04-09-operational",
+    title: "TrainLCD全サービスの提供再開",
+    description:
+      "2024年4月9日現在より自動アナウンス機能、フィードバック機能どちらとも使用可能となり、TrainLCDサービスの全面再開となりました。ご不便ご迷惑をおかけして申し訳ございませんでした。また皆様のメンテナンスへのご理解ご協力を心より感謝致します。",
+    publishedAt: "2024-04-09T00:00:00.000Z",
+    updatedAt: "2024-04-09T00:00:00.000Z",
+    resolvedAt: "2024-04-09T00:00:00.000Z",
+    estimatedResolveDate: null,
+    incidentImpact: "operational",
+  },
+  {
     id: "urn:uuid:3f00fc27-b12e-4cef-9899-ab6f63b46444",
     slug: "2",
     title: "TrainLCD一部サービスの提供再開",
@@ -93,7 +103,7 @@ export const incidentHistories: IncidentHistory[] = [
       "試験的にTrainLCDのサービスを一部再開しております。サービス全面再開ではございませんので、フィードバックと自動アナウンス機能はご利用になれません。また予期しないバグが発生する可能性がございます。",
     publishedAt: "2024-04-01T00:00:00.000Z",
     updatedAt: "2024-04-01T00:00:00.000Z",
-    resolvedAt: null,
+    resolvedAt: "2024-04-09T00:00:00.000Z",
     estimatedResolveDate: null,
     incidentImpact: "degraded",
   },
