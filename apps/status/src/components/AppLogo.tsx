b9990fb490fb1c982684a5f179b8f86450d5a38c
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-10-17 12:41:01 +0900

    SVGの不要なフィルタを削除しLCPを改善 (#63)
    * SVGの不要なフィルタを削除しLCPを改善
    
    * Ringのシンプル化と軽微なパフォーマンス改善

--- apps/status/src/components/AppLogo.tsx
+++ apps/status/src/components/AppLogo.tsx
@@ -11,7 +11,7 @@ const AppLogo = (props: Props) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <g filter="url(#filter0_d_4_10)">
+    <g>
       <rect x="4" width="592" height="752" rx="84" fill="#231E1F" />
       <rect
         x="68"
@@ -52,43 +52,6 @@ const AppLogo = (props: Props) => (
         fill="black"
       />
     </g>
-    <defs>
-      <filter
-        id="filter0_d_4_10"
-        x="0"
-        y="0"
-        width="600"
-        height="760"
-        filterUnits="userSpaceOnUse"
-        colorInterpolationFilters="sRGB"
-      >
-        <feFlood floodOpacity="0" result="BackgroundImageFix" />
-        <feColorMatrix
-          in="SourceAlpha"
-          type="matrix"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-          result="hardAlpha"
-        />
-        <feOffset dy="4" />
-        <feGaussianBlur stdDeviation="2" />
-        <feComposite in2="hardAlpha" operator="out" />
-        <feColorMatrix
-          type="matrix"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-        />
-        <feBlend
-          mode="normal"
-          in2="BackgroundImageFix"
-          result="effect1_dropShadow_4_10"
-        />
-        <feBlend
-          mode="normal"
-          in="SourceGraphic"
-          in2="effect1_dropShadow_4_10"
-          result="shape"
-        />
-      </filter>
-    </defs>
   </svg>
 );
 
