7a7a04ef7cd4cdb41135b42edfa6083753b5118f
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-10-17 11:07:12 +0900

    SSG推進部長

--- apps/lp/src/components/sections/Welcome.tsx
+++ apps/lp/src/components/sections/Welcome.tsx
@@ -1,57 +1,39 @@
-import styles from './Welcome.module.css';
 import mockImage from '../../assets/images/mockup/iphone-and-ipad.png';
 import ImageWithoutSize from '../ImageWithoutSize';
 import RingsPC from '../RingsPC';
 import RingsSP from '../RingsSP';
+import styles from './Welcome.module.css';
 
-const WelcomeSection = () => {
-  const handleTryButtonClick = () => {
-    const aboutElem = document.querySelector('#download');
-    window.scrollTo({
-      top: (aboutElem?.getBoundingClientRect().top ?? 0) + window.pageYOffset,
-      behavior: 'smooth',
-    });
-  };
-
-  return (
-    <section className={styles.container}>
-      <div className={styles.texts}>
-        <h2 className={styles.heading}>
-          日本全国の鉄道路線で使える
-          <br />
-          <span className={styles.colored}>新感覚</span>
-          ナビゲーションアプリ
-        </h2>
-        <p className={styles.description}>
-          今までにありそうでなかった
-          あなたのスマートフォンで使えるトレインビジョン。
-          <br />
-          それが、新感覚ナビゲーションアプリ「TrainLCD」
-          <br />
-          迷いそうな時、降りれるか不安な時。きっとあなたの役に立つはずです。
-        </p>
-        <button
-          type="button"
-          className={`${styles.button} ${styles.desktopButton}`}
-          onClick={handleTryButtonClick}
-        >
-          使ってみる
-        </button>
-      </div>
-      <div className={styles.mockup}>
-        <RingsPC className={styles.ringsPc} />
-        <RingsSP className={styles.ringsSp} />
-        <ImageWithoutSize src={mockImage} alt="iPhone and iPad" />
-      </div>
-      <button
-        type="button"
-        className={`${styles.button} ${styles.mobileButton}`}
-        onClick={handleTryButtonClick}
-      >
+const WelcomeSection = () => (
+  <section className={styles.container}>
+    <div className={styles.texts}>
+      <h2 className={styles.heading}>
+        日本全国の鉄道路線で使える
+        <br />
+        <span className={styles.colored}>新感覚</span>
+        ナビゲーションアプリ
+      </h2>
+      <p className={styles.description}>
+        今までにありそうでなかった
+        あなたのスマートフォンで使えるトレインビジョン。
+        <br />
+        それが、新感覚ナビゲーションアプリ「TrainLCD」
+        <br />
+        迷いそうな時、降りれるか不安な時。きっとあなたの役に立つはずです。
+      </p>
+      <a href="#download" className={`${styles.button} ${styles.desktopButton}`}>
         使ってみる
-      </button>
-    </section>
-  );
-};
+      </a>
+    </div>
+    <div className={styles.mockup}>
+      <RingsPC className={styles.ringsPc} />
+      <RingsSP className={styles.ringsSp} />
+      <ImageWithoutSize src={mockImage} alt="iPhone and iPad" />
+    </div>
+    <a href="#download" className={`${styles.button} ${styles.mobileButton}`}>
+      使ってみる
+    </a>
+  </section>
+);
 
 export default WelcomeSection;
