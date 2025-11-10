fb36b34b0447fadfcf5513a3e9f41eb87f670bd4
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-11-10 20:57:41 +0900

    カバレッジ設定追加

--- apps/status/vitest.config.ts
+++ apps/status/vitest.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vitest/config';
-import path from 'node:path';
+import { defineConfig } from "vitest/config";
+import path from "node:path";
 
 export default defineConfig({
   test: {
-    environment: 'node',
+    environment: "node",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
     deps: {
-      inline: ['data'],
+      inline: ["data"],
     },
   },
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@icons': path.resolve(__dirname, 'src/components/icons'),
-      '@pages': path.resolve(__dirname, 'src/components/pages'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@icons": path.resolve(__dirname, "src/components/icons"),
+      "@pages": path.resolve(__dirname, "src/components/pages"),
+      "@utils": path.resolve(__dirname, "src/utils"),
     },
   },
 });
